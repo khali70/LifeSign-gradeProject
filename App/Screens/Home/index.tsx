ad73fd4a4c6867de22cf92a5800b5c4a370e8136
khali70 <k7alifa.a7med70@gmail.com>
2021-09-06 01:25:33 +0200

    Call 911

--- App/Screens/Home/index.tsx
+++ App/Screens/Home/index.tsx
@@ -26,7 +26,7 @@ import I18n from '../../i18n';
 import useTts from './useTts';
 import Warper from '../../components/HeaderWarper';
 import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
-
+import {Linking} from 'react-native';
 export default () => {
   const {state, actions} = useTts();
   // FIXME not the best practice to setState
@@ -43,7 +43,6 @@ export default () => {
     setText(tts);
     actions.readText();
   };
-  //
   return (
     <Warper title={I18n.t('Speak')} root>
       <Layout style={styles.container}>
@@ -52,17 +51,18 @@ export default () => {
           <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <Input value={tts} onChangeText={e => setTTs(e)} />
             <Button
-              style={styles.goHome}
-              accessoryLeft={<Icon name="home" />}
-              onPress={() => navigation.push('Listen')}>
-              {I18n.t('GoListen')}
-            </Button>
-            <Button
               style={styles.speak}
               accessoryLeft={<Icon name="volume-up" />}
               onPress={() => speak()}>
               {I18n.t('Speak')}
             </Button>
+            <Button
+              style={styles.goHome}
+              accessoryLeft={<Icon name="home" />}
+              onPress={() => navigation.push('Listen')}>
+              {I18n.t('GoListen')}
+            </Button>
+            <Button onPress={() => Linking.openURL(`tel:911`)}>call 911</Button>
           </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
       </Layout>
@@ -79,10 +79,10 @@ const styles = StyleSheet.create({
   text: {
     textAlign: 'center',
   },
-  goHome: {
+  speak: {
     marginVertical: 16,
   },
-  speak: {
+  goHome: {
     marginVertical: 16,
     paddingHorizontal: 20,
   },
