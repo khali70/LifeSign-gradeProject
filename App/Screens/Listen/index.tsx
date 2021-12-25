edfa184e3279962201ceb4d119062dee3fa2bc4a
khali70 <k7alifa.a7med70@gmail.com>
2021-12-25 15:04:19 +0200

    link: stt lib

--- App/Screens/Listen/index.tsx
+++ App/Screens/Listen/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {Alert, SafeAreaView, StyleSheet} from 'react-native';
 
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp, StackScreenProps} from '@react-navigation/stack';
@@ -14,15 +14,64 @@ import {
 } from '@ui-kitten/components';
 import I18n from '@i18n/index';
 import Warper from '@components/HeaderWarper';
+import Voice from '@react-native-voice/voice';
 
 type props = StackScreenProps<HomeStackPrams,'Listen'>;
 export default ({navigation,route}:props) => {
+  useEffect(() => {
+    initFunction()
+}, []);
+const initFunction =  async () =>{
+  if(await Voice.isAvailable()){
+    Voice.onSpeechStart =(e)=>{
+      console.tron.log("onSpeechStart",e)
+    };
+    Voice.onSpeechRecognized =(e)=>{
+      console.tron.log("onSpeechRecognized",e)
+    };
+    Voice.onSpeechEnd =(e)=>{
+      console.tron.log("onSpeechEnd",e)
+    };
+    Voice.onSpeechError =(e)=>{
+      console.tron.log("onSpeechError",e)
+    };
+    Voice.onSpeechResults =(e)=>{
+      console.tron.log("onSpeechResults",e)
+    };
+    Voice.onSpeechPartialResults =(e)=>{
+      console.tron.log("onSpeechPartialResults",e)
+    };
+    Voice.onSpeechVolumeChanged =(e)=>{
+      console.tron.log("onSpeechVolumeChanged",e)
+    };      
+  }else{
+    Alert.alert('stt not found',`speech to text is not available on this device`,[
+      {
+        text:"ok",
+        style:"cancel"
+      }
+    ])
+  }
+}
+    /**
+   * ! google speak to text
+   * ! load image to text
+   */
+  const listen = async ()=>{
+   await Voice.start('ar');
+   setTimeout(()=>{
+     Voice.stop()
+   },1000)
+  }
   return (
     <Warper title={I18n.t(route.name)}>
       <Layout style={styles.container}>
         <Text style={styles.text} category="h1">
           Soon listen mod
         </Text>
+        <Button onPress={listen}> 
+        listen
+        </Button>
         <Button
           accessoryLeft={<Icon name="home" />}
           style={styles.likeButton}
--- android/app/src/main/java/com/lifesign/MainApplication.java
+++ android/app/src/main/java/com/lifesign/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.wenkesj.voice.VoicePackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import net.no_mad.tts.TextToSpeechPackage;
 import com.facebook.react.ReactInstanceManager;
